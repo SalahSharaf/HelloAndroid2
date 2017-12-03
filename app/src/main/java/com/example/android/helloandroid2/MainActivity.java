99da6deafb790c1f59af7beacdb1cd559836ecf4
Salah Sharaf ElDeen <33465920+SalahSharaf@users.noreply.github.com>
2017-12-04 00:26:11 +0200

    this is the anim folder i have added to my project

--- app/src/main/java/com/example/android/helloandroid2/MainActivity.java
+++ app/src/main/java/com/example/android/helloandroid2/MainActivity.java
@@ -8,18 +8,25 @@ import android.content.pm.ResolveInfo;
 import android.net.Uri;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.List;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends FragmentActivity implements Animation.AnimationListener {
+
+    Animation anim1, anim2, anim3, anim4,anim5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        animationTask();
     }
 
     public void webIntent(View view) {
@@ -37,41 +44,46 @@ public class MainActivity extends FragmentActivity {
             startActivity(intent);
         }
     }
-    public  void googleMapsIntent(View view){
+
+    public void googleMapsIntent(View view) {
         Uri gmmIntentUri = Uri.parse("google.navigation:q=an+2465 Latham St+Mountain View");
         Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
         mapIntent.setPackage("com.google.android.apps.maps");
         if (mapIntent.resolveActivity(getPackageManager()) != null) {
             startActivity(mapIntent);
-        }else {
-            Toast.makeText(this,"Sorry Something wrong has happend check if your device has google maps instaled",Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "Sorry Something wrong has happened check if your device has google maps instaled", Toast.LENGTH_LONG).show();
         }
 
     }
-    public  void facebookIntent(View view){
-        String uri=getFacebookPageURL(this);
-        Intent intent=new Intent(Intent.ACTION_VIEW);
+
+    public void facebookIntent(View view) {
+        String uri = getFacebookPageURL(this);
+        Intent intent = new Intent(Intent.ACTION_VIEW);
         intent.setData(Uri.parse(uri));
-        if(intent.resolveActivity(getPackageManager())!=null){
+        if (intent.resolveActivity(getPackageManager()) != null) {
             startActivity(intent);
         }
     }
-    public void twitterIntent(View view){
-        Intent intent=new Intent(Intent.ACTION_VIEW);
+
+    public void twitterIntent(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
         intent.setData(Uri.parse("https://twitter.com/udacity?lang=ar"));
-        if(intent.resolveActivity(getPackageManager())!=null){
+        if (intent.resolveActivity(getPackageManager()) != null) {
             startActivity(intent);
         }
     }
-    public void linkedinIntent(View view){
+
+    public void linkedInIntent(View view) {
         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("linkedin://group/[164070]"));
-        final PackageManager packageManager = getApplicationContext().getPackageManager();
-        final List<ResolveInfo> list = packageManager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
+        PackageManager packageManager = getApplicationContext().getPackageManager();
+        List<ResolveInfo> list = packageManager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
         if (list.isEmpty()) {
             intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.linkedin.com/edu/udacity-164070"));
         }
         startActivity(intent);
     }
+
     public String getFacebookPageURL(Context context) {
         PackageManager packageManager = context.getPackageManager();
         try {
@@ -84,5 +96,87 @@ public class MainActivity extends FragmentActivity {
         } catch (PackageManager.NameNotFoundException e) {
             return "https://www.facebook.com/Udacity"; //normal web url
         }
+    }
+
+    @Override
+    public void onAnimationStart(Animation animation) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animation animation) {
+
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+
+    }
+    void animationTask(){
+        anim1 = AnimationUtils.loadAnimation(this, R.anim.animation1);
+        anim2 = AnimationUtils.loadAnimation(this, R.anim.move);
+        anim2.setDuration(2000);
+        anim3 = AnimationUtils.loadAnimation(this, R.anim.move);
+        anim3.setDuration(2200);
+        anim4 = AnimationUtils.loadAnimation(this, R.anim.move);
+        anim4.setDuration(2400);
+        anim5=AnimationUtils.loadAnimation(this,R.anim.move2);
+        ImageView linkedin = findViewById(R.id.linkedin);
+        ImageView twitter = findViewById(R.id.twitter);
+        ImageView facebook = findViewById(R.id.facebook);
+        TextView text = findViewById(R.id.address);
+        TextView text2 = findViewById(R.id.phone);
+        TextView text3 = findViewById(R.id.web);
+        linkedin.startAnimation(anim5);
+        facebook.startAnimation(anim5);
+        twitter.startAnimation(anim5);
+        linkedin.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    v.startAnimation(anim1);
+                    return true;
+                }
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    linkedInIntent(v);
+                    return true;
+                }
+                return false;
+            }
+        });
+        facebook.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    v.startAnimation(anim1);
+                    return true;
+                }
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    facebookIntent(v);
+                    return true;
+                }
+                return false;
+            }
+
+        });
+        twitter.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    v.startAnimation(anim1);
+                    return true;
+                }
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    twitterIntent(v);
+                    return true;
+                }
+                return false;
+            }
+        });
+        text.startAnimation(anim2);
+        text2.startAnimation(anim3);
+        text3.startAnimation(anim4);
     }
 }
